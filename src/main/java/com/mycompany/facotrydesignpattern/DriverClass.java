85f8cdb6135d036c3c24898155f5494f674c8f46
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2022-12-04 22:25:01 -0500

    Changed to 8k TV

--- src/main/java/com/mycompany/facotrydesignpattern/DriverClass.java
+++ src/main/java/com/mycompany/facotrydesignpattern/DriverClass.java
@@ -6,8 +6,8 @@ public class DriverClass {
     
     public static void main(String[] args) {
         
-        // use the factory class to get an instance of a 4k tv
-        TV myTV = new TVSetFactory().getInstance("4K");
+        // use the factory class to get an instance of a 8k tv
+        TV myTV = new TVSetFactory().getInstance("8K");
         
         myTV.specs();
     }
